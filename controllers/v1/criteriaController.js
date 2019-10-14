b39837410f4cc991f95713f55a608e969f85d5db
ajithkumar <ajith@tunerlabs.com>
2019-10-14 14:13:06 +0530

    forgot to add this file

--- controllers/v1/criteriaController.js
+++ controllers/v1/criteriaController.js
@@ -3,6 +3,18 @@ const FileStream = require(ROOT_PATH + "/generics/fileStream");
 
 module.exports = class Criteria extends Abstract {
 
+  /**
+   * @apiDefine errorBody
+   * @apiError {String} status 4XX,5XX
+   * @apiError {String} message Error
+   */
+
+  /**
+   * @apiDefine successBody
+   *  @apiSuccess {String} status 200
+   * @apiSuccess {String} result Data
+   */
+
   constructor() {
     super(criteriaSchema);
   }
